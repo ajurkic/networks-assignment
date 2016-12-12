0f6d75f76e9d836711763513b1edf08d3225fe8b
poohpack <poohpack@mailinator.com>
2016-12-12 23:31:16 +0100

    kk

--- crc32.py
+++ crc32.py
@@ -1,5 +1,5 @@
 import binascii
-#komentar
+
 def create_table():
     a = []
     for i in range(256):
@@ -8,6 +8,7 @@ def create_table():
             if k & 1:
                 k ^= 0x1db710640
             k >>= 1
+            print(k)
         a.append(k)
     return a
  
@@ -18,5 +19,4 @@ def crc_update(buf, crc):
     return crc ^ 0xffffffff
  
 crc_table = create_table()
-print(crc_table)
 print(hex(crc_update(b"hello-world", 0)))
